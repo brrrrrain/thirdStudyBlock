dfdc7ab835fca297d5a8244c49174e90d8512f39
Ilya <tenishev22099@gmail.com>
2018-12-20 01:46:59 +0300

    added a project to reposition

---
+++ app/src/main/java/com/example/hp/thenewestproject/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.hp.thenewestproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.hp.thenewestproject.Sort.Sort;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int[] arr = {2, 4, 1, 5, 6, 3};
+    private int[] arr1 = {5, 4, 7, 1, 2, 3, 1, 8};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tvResult = findViewById(R.id.tvResult);
+        TextView newText = findViewById(R.id.tvAnotherResult);
+        Button btn = findViewById(R.id.btnId);
+
+        Sort newSort = new Sort();
+        int[] sortMass = newSort.bubbleSort(arr);
+        int[] newSortMass = newSort.choiseSort(arr1);
+
+        tvResult.setText(Arrays.toString(sortMass));
+        newText.setText(Arrays.toString(newSortMass));
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "textText", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/hp/thenewestproject/Sort/Sort.java
+++ app/src/main/java/com/example/hp/thenewestproject/Sort/Sort.java
@@ -1,4 +1,49 @@
-package PACKAGE_NAME;
+package com.example.hp.thenewestproject.Sort;
 
 public class Sort {
+    public int[] bubbleSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+        }
+        for (int i = array.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temporary = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temporary;
+                }
+            }
+        }
+        return array;
+
+    }
+
+    public int[] choiseSort(int[] array1) {
+        for (int i = 0; i < array1.length; i++) {
+            System.out.print(array1[i]);
+        }
+
+        for (int i = 0; i < array1.length; i++) {
+            int index = i;
+            int element = array1[i];
+            for (int j = i + 1; j < array1.length; j++) {
+                if (array1[j] < element) {
+                    index = j;
+                    element = array1[j];
+                }
+                //array1[index] = array1[i];
+                //array1[i] = element;
+            }
+            if (i != index){
+                int temp = array1[i];
+                array1[i] = array1[index];
+                array1[index] = temp;
+            }
+        }
+
+        for (int i = 0; i < array1.length; i++) {
+            System.out.print(array1[i]);
+        }
+        return array1;
+    }
 }
